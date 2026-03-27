5b8daf95e3a44c7df13ec7118c18fcd694f21db2
vexbyr <ikkot8143@gmail.com>
2026-03-27 23:32:25 +0500

    Add files via upload

---
+++ bookstore/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"bookstore/handlers"
+	"bookstore/models"
+)
+
+func main() {
+	handlers.AuthorsInit([]models.Author{
+		{Name: "Isaac Asimov"},
+		{Name: "J.K. Rowling"},
+		{Name: "George Orwell"},
+	})
+
+	handlers.CategoriesInit([]models.Category{
+		{Name: "Science Fiction"},
+		{Name: "Fantasy"},
+		{Name: "Dystopia"},
+	})
+
+	handlers.BooksInit([]models.Book{
+		{Title: "Foundation", AuthorID: 1, CategoryID: 1, Price: 12.99},
+		{Title: "Harry Potter", AuthorID: 2, CategoryID: 2, Price: 9.99},
+		{Title: "1984", AuthorID: 3, CategoryID: 3, Price: 11.50},
+	})
+
+	http.HandleFunc("/books", handlers.BooksHandler)
+	http.HandleFunc("/books/", handlers.BookByIDHandler)
+
+	http.HandleFunc("/authors", handlers.AuthorsHandler)
+	http.HandleFunc("/categories", handlers.CategoriesHandler)
+
+	log.Println("Server started on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
